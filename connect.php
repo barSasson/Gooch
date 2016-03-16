cc7ed8632b8e292a485179486771c6d2697a37aa
barSasson <barsasson@MacBook-Pro.local>
2016-03-16 03:35:43 +0200

    df

--- connect.php
+++ connect.php
@@ -5,8 +5,30 @@ $password = "f_kHaNi63ccf";
 
 // Create connection
 $db = mysql_connect($servername,$username,$password);
+
+
+
+
  if (!$db) {
  die("Database connection failed miserably: " . mysql_error());
  }
-echo "sdfsdf";
+
+
+ $sql = "SHOW TABLES FROM $db";
+ $result = mysql_query($sql);
+
+
+
+ if (!$result) {
+    echo "DB Error, could not list tables\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
+
+while ($row = mysql_fetch_row($result)) {
+    echo "Table: {$row[0]}\n";
+}
+
 ?>
+
+

774dff24c5933c46e86946310cb0b77db13a50e7
barSasson <barsasson@MacBook-Pro.local>
2016-03-16 03:29:46 +0200

    df

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "127.9.124.2:3306";
+$username = "adminUbfgc62";
+$password = "f_kHaNi63ccf";
+
+// Create connection
+$db = mysql_connect($servername,$username,$password);
+ if (!$db) {
+ die("Database connection failed miserably: " . mysql_error());
+ }
+
+?>

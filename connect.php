1e94f126f9adfdadda029340570c1bbe41932643
barSasson <barsasson@MacBook-Pro.local>
2016-03-18 04:27:56 +0200

    dd

--- connect.php
+++ connect.php
@@ -2,8 +2,12 @@
 $servername = "127.0.0.1:3306";
 $username = "adminUbfgc62";
 $password = "f_kHaNi63ccf";
+
 // Create connection
 $db = mysql_connect($servername,$username,$password);
+
+$m = new MongoClient();
+echo $m;
  if (!$db) {
  die("Database connection failed miserably: " . mysql_error());
  }

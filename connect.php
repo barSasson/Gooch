6e8da2473e94e9fe0a7035a479358f4dfab039da
barSasson <barsasson@MacBook-Pro.local>
2016-03-18 05:30:41 +0200

    ddjj

--- connect.php
+++ connect.php
@@ -6,12 +6,13 @@ $password = "f_kHaNi63ccf";
 // Create connection
 //$db = mysql_connect($servername,$username,$password);
 
-$m = new MongoClient("mongodb://admin:eTbFI2KfnvTI@127.9.124.4:27017");
-$d = $m->selectCollection("bb");
-echo $d;
- if($m->connected)
-    echo "Connected successfully";
-else
-    echo "Connection failed";
+$db = new MongoClient("mongodb://admin:eTbFI2KfnvTI@127.9.124.4:27017");
+
+$collections = $db->listCollections();
+
+foreach ($collections as $collection) {
+    echo "amount of documents in $collection: ";
+    echo $collection->count(), "\n";
+}
 
 ?>

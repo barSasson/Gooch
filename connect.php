416effd681ccc49ae6878e5929a2ff618f358f0c
barSasson <barsasson@MacBook-Pro.local>
2016-03-18 05:18:04 +0200

    dd

--- connect.php
+++ connect.php
@@ -1,5 +1,14 @@
 <?php
-$s = new Mongo();	
+$servername = "127.0.0.1:3306";
+$username = "adminUbfgc62";
+$password = "f_kHaNi63ccf";
 
+// Create connection
+//$db = mysql_connect($servername,$username,$password);
+
+$m = new MongoClient("127.9.124.4:27017");
+$d=$m->bb;
+echo $d;
+ 
 
 ?>

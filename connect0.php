32fe990b83e6f257da89416f89602049a9d286e2
barSasson <barsasson@MacBook-Pro.local>
2016-04-26 17:11:59 +0300

    ddd

--- connect0.php
+++ connect0.php
@@ -2,13 +2,10 @@
 $servername = "127.9.124.2:3306";
 $server_login_username = "adminUbfgc62";
 $server_login_password = "f_kHaNi63ccf";
-$temp_pass= "2565121024";
-$id =1;
-
+$safe_username_input = mysql_real_escape_string($_POST['username-input']);
+$safe_password_input = $_POST['password-input'];
 // Create connection
 $db = mysql_connect($servername,$server_login_username,$server_login_password);
-
-
 
 
  if (!$db) {
@@ -16,12 +13,12 @@ $db = mysql_connect($servername,$server_login_username,$server_login_password);
  }
 mysql_select_db('dizz');
 
- $sql = "SELECT password FROM users where id=".$id;
+ $sql = "SELECT password FROM users where username=".$safe_username_input;
  $result = mysql_query($sql);
 
 $row = mysql_fetch_assoc($result);
 
-if(hash("sha256", $temp_pass) == $row['password'])
+if(hash("sha256", $safe_password_input) == $row['password'] && $row['password'] && $safe_password_input)
 {
    echo "login successful";
 }

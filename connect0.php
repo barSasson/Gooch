2f24169d62e873da13ca15a4e138b1ed36c3efe8
barSasson <barsasson@MacBook-Pro.local>
2016-04-26 17:58:44 +0300

    ddd

--- connect0.php
+++ connect0.php
@@ -2,27 +2,33 @@
 $servername = "127.9.124.2:3306";
 $server_login_username = "adminUbfgc62";
 $server_login_password = "f_kHaNi63ccf";
-
-
-
+$database_name = "dizz";
 // Create connection
-$db = mysql_connect($servername, $server_login_username, $server_login_password);
+$server_connect_response = mysql_connect($servername, $server_login_username, $server_login_password);
+
+if (!$server_connect_response) {
+ die("Database connection failed " . mysql_error());
+ }
+ 
+mysql_select_db($database_name);
+
+
 
 $safe_username_input = mysql_real_escape_string($_POST["username-input"]);
 $safe_password_input = $_POST["password-input"];
 $input_was_given = isset($_POST["password-input"]) && isset($_POST["username-input"]);
 
 
- if (!$db) {
- die("Database connection failed miserably: " . mysql_error());
- }
-mysql_select_db('dizz');
+$sql_query = "SELECT password FROM users WHERE username='".$safe_username_input."'";
+$query_result = mysql_query($sql_query);
+if (!$query_result) {
+    echo "DB Error, could not process the query\n";
+    echo 'MySQL Error: ' . mysql_error();
+    exit;
+}
 
- $sql = "SELECT password FROM users WHERE username='".$safe_username_input."'";
- $result = mysql_query($sql);
-
-$row = mysql_fetch_assoc($result);
-$password_is_matching = hash("sha256", $safe_password_input) == $row['password'];
+$first_row_in_query_result = mysql_fetch_assoc($query_result);
+$password_is_matching = hash("sha256", $safe_password_input) == $first_row_in_query_result['password'];
 
 if($input_was_given && $password_is_matching)
 {
@@ -34,11 +40,7 @@ else
 }
 
 
- if (!$result) {
-    echo "DB Error, could not list tables\n";
-    echo 'MySQL Error: ' . mysql_error();
-    exit;
-}
+ mysql_close($server_connect_response);
 
 ?>
 

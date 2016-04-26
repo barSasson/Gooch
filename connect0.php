e29e743270f57b57a6dbf9ceae7539e79fe82046
barSasson <barsasson@MacBook-Pro.local>
2016-04-26 16:35:47 +0300

    ddd

--- connect0.php
+++ connect0.php
@@ -2,8 +2,7 @@
 $servername = "127.9.124.2:3306";
 $username = "adminUbfgc62";
 $password = "f_kHaNi63ccf";
-$id = 1;
-$pass = "2565121024";
+$id =1;
 // Create connection
 $db = mysql_connect($servername,$username,$password);
 
@@ -13,33 +12,15 @@ $db = mysql_connect($servername,$username,$password);
  if (!$db) {
  die("Database connection failed miserably: " . mysql_error());
  }
-
 mysql_select_db('dizz');
 
-
-//-----------------
- $sql = "SELECT * FROM users";
+ $sql = "SELECT password FROM users where id=1";
  $result = mysql_query($sql);
 
-while ($row = mysql_fetch_assoc($result))
+while($row = mysql_fetch_assoc($result))
 {
-	echo $row[2];
+   print_r($row);
 }
-
-$hashed = hash('sha256', $pass); 
-//--------------------
-echo "-----";
-
- $sql = "SELECT * FROM users WHERE id='1'";
- $result = mysql_query($sql);
-
-$row = mysql_fetch_assoc($result);
-while ($row = mysql_fetch_assoc($result))
-{
-	echo $row[2];
-}
-
-$hashed = hash('sha256', $pass); 
 
 
  if (!$result) {

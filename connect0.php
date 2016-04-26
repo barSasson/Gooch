4d22b1fb9705adc21b02d481dd022991d5745032
barSasson <barsasson@MacBook-Pro.local>
2016-04-26 17:48:59 +0300

    ddd

--- connect0.php
+++ connect0.php
@@ -8,9 +8,9 @@ $server_login_password = "f_kHaNi63ccf";
 // Create connection
 $db = mysql_connect($servername, $server_login_username, $server_login_password);
 
-$safe_username_input = $_POST["username-input"];
+$safe_username_input = mysql_real_escape_string($_POST["username-input"]);
 $safe_password_input = $_POST["password-input"];
-$password_input_was_given = isset($_POST["password-input"]);
+$input_was_given = isset($_POST["password-input"]) && isset($_POST["username-input"]);
 
 
  if (!$db) {
@@ -24,15 +24,13 @@ mysql_select_db('dizz');
 $row = mysql_fetch_assoc($result);
 $password_is_matching = hash("sha256", $safe_password_input) == $row['password'];
 
-if($password_input_was_given && $password_is_matching)
+if($input_was_given && $password_is_matching)
 {
    echo "login successful";
 }
 else
 {
-	echo $row['password'].'|';
-	echo $_POST["password-input"].'|';
-	echo $_POST["username-input"].'|';
+	echo "login failed";
 }
 
 

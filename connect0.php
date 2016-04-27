1a1292580e6696e9aa00641dd3396d48ebd19c5b
barSasson <barsasson@MacBook-Pro.local>
2016-04-27 12:02:05 +0300

    ddd

--- connect0.php
+++ connect0.php
@@ -19,8 +19,10 @@ $safe_password_input = $_POST["password-input"];
 $input_was_given = isset($_POST["password-input"]) && isset($_POST["username-input"]);
 
 
-$sql_query = "SELECT password FROM users WHERE username='".$safe_username_input."'";
-$query_result = mysql_query($sql_query);
+$sql_query = "SELECT password,id FROM users WHERE username=?";
+$params = array($safe_username_input);
+$query_result = sqlsrv_query($server_connect_response, $sql_query, $params);
+
 if (!$query_result) {
     echo "DB Error, could not process the query\n";
     echo 'MySQL Error: ' . mysql_error();
@@ -32,7 +34,10 @@ $password_is_matching = hash("sha256", $safe_password_input) == $first_row_in_qu
 
 if($input_was_given && $password_is_matching)
 {
-   echo "login successful";
+   $_SESSION['loggedin'] = true;
+   $_SESSION['user_id'] = $first_row_in_query_result['id'];
+   header("Location: ./index.php?login_failed");
+   
 }
 else
 {

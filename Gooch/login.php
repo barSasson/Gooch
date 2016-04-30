abdd95c8ce07c3f2cc0729f9f81f896742107f63
barSasson <barsasson@MacBook-Pro.local>
2016-04-30 17:01:19 +0300

    ddd

--- Gooch/login.php
+++ Gooch/login.php
@@ -6,28 +6,13 @@ $safe_password_input = $_POST["password-input"];
 $input_was_given = isset($_POST["password-input"]) && isset($_POST["username-input"]);
 
 
-$sql_query = "SELECT password,id FROM users WHERE username='".$safe_username_input."'";
+$sql_query = "SELECT email FROM users";
 $query_result = mysql_query($sql_query);
-if (!$query_result) {
-    echo "DB Error, could not process the query\n";
-    echo 'MySQL Error: ' . mysql_error();
-    exit;
-}
-
-$first_row_in_query_result = mysql_fetch_assoc($query_result);
-$password_is_matching = hash("sha256", $safe_password_input) == $first_row_in_query_result['password'];
-
-if($input_was_given && $password_is_matching)
-{
-   session_start();
-   $_SESSION['loggedin'] = true;
-   $_SESSION['user_id'] = $first_row_in_query_result['id'];
-   header("Location: ./addshift.php");
-}
-else
-{
-header("Location: ./index.php?login_failed");
-}
+echo mysql_num_rows($query_result);
+var_dump($query_result);
+$row = mysql_fetch_assoc($result);
+print_r($row);
+var_dump($row);
 
 
  mysql_close($server_connect_response);

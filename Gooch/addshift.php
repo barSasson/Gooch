261064dd531ed1f287e67e849fca684ec2632572
barSasson <barsasson@MacBook-Pro.local>
2016-04-30 16:09:07 +0300

    ddd

--- Gooch/addshift.php
+++ Gooch/addshift.php
@@ -1,20 +1,10 @@
 <?php
-session_start();
+
+require_once('config.php'); 
+
+
 if(!isset($_SESSION["loggedin"]))
 header("Location: ./index.php");
-
-$servername = "127.9.124.2:3306";
-$server_login_username = "adminUbfgc62";
-$server_login_password = "f_kHaNi63ccf";
-$database_name = "dizz";
-// Create connection
-$server_connect_response = mysql_connect($servername, $server_login_username, $server_login_password);
-
-if (!$server_connect_response) {
- die("Database connection failed " . mysql_error());
- }
- 
-mysql_select_db($database_name);
 
 
 $sql_query = "SELECT password,id FROM users";

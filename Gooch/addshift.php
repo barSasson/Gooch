021ed4bd1cafc2cc4ef828feb38adb075d008b2f
barSasson <bar.sasson@gmail.com>
2016-05-05 16:05:40 +0300

    ddd

--- Gooch/addshift.php
+++ Gooch/addshift.php
@@ -388,7 +388,7 @@ input[type=range]:focus::-ms-fill-upper {
 						<div class="container">
 							<p>Date</p>
 							<div id="datePickerContainer">
-								<input id='datePicker' data-provide='datepicker' data-date-container='#datePickerContainer' class="datepicker form-control default-input-style input-md" type="text" style='width:70%; text-align: center; color: #ffffff'>
+								<input id='datePicker' data-provide='datepicker' data-date-container='#datePickerContainer' class="datepicker form-control default-input-style input-md" type="text" style='width:60%; text-align: center; color: #ffffff'>
 							</div>
 						</div>
 						<div class="container">

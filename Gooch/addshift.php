b1cebaa17c753de36f46995abb5c6e423423c59d
barSasson <bar.sasson@gmail.com>
2016-05-05 15:22:33 +0300

    ddd

--- Gooch/addshift.php
+++ Gooch/addshift.php
@@ -406,10 +406,10 @@ input[type=range]:focus::-ms-fill-upper {
 								</label>
 							</div>
 							<div class="btn-group" data-toggle="buttons">
-								<label class="btn default-input-style active">
+								<label class="btn default-input-style active col-xs-6">
 									<input type="radio" name="checker-exists" id="checker-exists-input" checked autocomplete="off"/><span>Checker Enabled</span>
 								</label>
-								<label class="btn default-input-style">
+								<label class="btn default-input-style col-xs-6">
 									<input type="radio" name="checker-exists" id="checker-not-exists-input" autocomplete="off"/><span>Checker Disabled</span>
 								</label>
 							</div>

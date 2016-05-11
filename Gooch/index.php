7e9eab04f16a86eac177e77557f754d00e4c0246
barSasson <bar.sasson@gmail.com>
2016-05-11 19:27:05 +0300

    test

--- Gooch/index.php
+++ Gooch/index.php
@@ -145,13 +145,13 @@ echo "</div>";
   };
 
 
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.6&appId=123851931358081";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
 
 // This is called with the results from from FB.getLoginStatus().
   function statusChangeCallback(response) {

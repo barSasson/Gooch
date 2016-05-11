2441cba6ded16edd9b14048edc2d2a5267ed8024
barSasson <bar.sasson@gmail.com>
2016-05-11 20:37:42 +0300

    test

--- Gooch/index.php
+++ Gooch/index.php
@@ -114,8 +114,6 @@ echo "<strong>Login Failed!</strong> The user name or password is incorrect";
 echo "</div>";
 }
 
-<?php
-
 $fb = new Facebook\Facebook(array('app_id' => '123851931358081','app_secret' => '5a3f6c3d3f10f796de6efbd88783b804','default_graph_version' => 'v2.5'));
 
 $helper = $fb->getRedirectLoginHelper();

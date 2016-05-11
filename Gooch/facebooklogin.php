8c9949a73455ba2d1b31b77c1fe37ef23f92b051
barSasson <bar.sasson@gmail.com>
2016-05-11 19:40:55 +0300

    test

--- Gooch/facebooklogin.php
+++ Gooch/facebooklogin.php
@@ -4,9 +4,7 @@
 <?php
 require_once __DIR__ . '/facebook-sdk-v5/autoload.php';
 $fb = new Facebook\Facebook(array('app_id' => '123851931358081','app_secret' => '5a3f6c3d3f10f796de6efbd88783b804','default_graph_version' => 'v2.5'));
-var_dump($fb);
 $helper = $fb->getJavaScriptHelper();
-var_dump($helper);
 
 try {
   $accessToken = $helper->getAccessToken();
@@ -19,8 +17,8 @@ try {
   echo 'Facebook SDK returned an error: ' . $e->getMessage();
   exit;
 }
-var_dump($helper);
 if (isset($accessToken)) {
+  echo "loggedin";
 	header("Location: ./addshift.php");
 }
 else

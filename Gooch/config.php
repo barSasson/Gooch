6c7ce87e6cc661b2b57c4ac61ed57be0a6ee131a
barSasson <bar.sasson@gmail.com>
2016-05-15 11:17:21 +0300

    test

--- Gooch/config.php
+++ Gooch/config.php
@@ -1,18 +1,15 @@
 <?php
-$servername = "localhost";
-$server_login_username = "root";
-$server_login_password = "";
-$database_name = "dizz";
+$servername = "127.7.203.2:3306";
+$server_login_username = "adminGeMF3HG";
+$server_login_password = "_kTYqdlwMtE9";
+$database_name = "tips";
 // Create connection
 $server_connect_response = mysql_connect($servername, $server_login_username, $server_login_password);
-
 if (!$server_connect_response) {
  die("Database connection failed " . mysql_error());
  }
  
 mysql_select_db($database_name);
 mysql_query("SET NAMES 'utf8'");
-
 ?>
-
 

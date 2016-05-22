fa35d3a8acc791cc234d82113c67bae5828887bf
barSasson <bar.sasson@gmail.com>
2016-05-23 02:01:08 +0300

    test

--- Gooch/login.php
+++ Gooch/login.php
@@ -1,11 +1,11 @@
 <?php
 require_once('config.php'); 
-$safe_username_input = mysql_real_escape_string($_POST["username-input"]);
+$safe_username_input = mysqli_real_escape_string($mysqli, $_POST["username-input"]);
 $safe_password_input = $_POST["password-input"];
 $input_was_given = isset($_POST["password-input"]) && isset($_POST["username-input"]);
 
 
-$sql_query = "SELECT password,id,username_heb FROM users WHERE username='gooch'";
+$sql_query = "SELECT password,id,username_heb FROM users WHERE username='".$safe_username_input."'";
 $query_result = mysqli_query($mysqli, $sql_query);
 if (!$query_result) {
     echo "DB Error, could not process the query\n";
@@ -15,10 +15,7 @@ if (!$query_result) {
 
 $first_row_in_query_result = mysqli_fetch_assoc($query_result);
 $password_is_matching = hash("sha256", $safe_password_input) == $first_row_in_query_result['password'];
-var_dump($first_row_in_query_result);
-var_dump($password_is_matching);
-var_dump($$first_row_in_query_result['password']);
-exit;
+
 if($input_was_given && $password_is_matching)
 {
    session_start();
@@ -29,6 +26,7 @@ if($input_was_given && $password_is_matching)
 }
 else
 {
+header("Location: ./index.php?login_failed");
 }
 
 

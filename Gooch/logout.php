124080a1b710741bdeb85ad9081cef87567bc4b5
barSasson <bar.sasson@gmail.com>
2016-05-23 01:44:46 +0300

    test

--- Gooch/logout.php
+++ Gooch/logout.php
@@ -10,7 +10,7 @@ if(isset($_SESSION['facebook_access_token']))
 		$fb = new Facebook\Facebook(array('app_id' => '123851931358081','app_secret' => '5a3f6c3d3f10f796de6efbd88783b804','default_graph_version' => 'v2.5'));
 		$helper = $fb->getRedirectLoginHelper();
 		$accessToken = $helper->getAccessToken();
-		$logoutUrl = 'https://www.facebook.com/logout.php?next=http://localhost/Gooch/logout-redirect.php&access_token='.$_SESSION['facebook_access_token'] ;
+		$logoutUrl = 'https://www.facebook.com/logout.php?next=https://tips-gooch.rhcloud.com/Gooch/logout-redirect.php&access_token='.$_SESSION['facebook_access_token'] ;
 		header("Location: ".$logoutUrl);	
 
 	}
